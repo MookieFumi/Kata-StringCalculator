8c644207bea15908ba5cb5358e9370a06cfb120c
Miguel Angel Martin Hrdez <miguel.martin@analyticalways.com>
2017-10-05 12:05:04 +0200

    Initial commit

--- StringCalculator/UnitTest1.cs
+++ StringCalculator/UnitTest1.cs
@@ -1,14 +1,26 @@
 ﻿using System;
-using System.Linq;
+using System.Threading;
+using FluentAssertions;
 using Xunit;
 
 namespace StringCalculator
 {
-    public class UnitTest1
+    public class StringCalculatorShould
     {
         [Fact]
-        public void TestMethod1()
+        public void return_0_if_param_is_an_empty_string()
         {
+            var sut = new StringCalculator();
+            var result = sut.Add(string.Empty);
+            result.Should().Be(0);
+        }
+    }
+
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            return 2;
         }
     }
 }
