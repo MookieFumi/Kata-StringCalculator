92183cab4ae02864e5e82b1896f3c0601ddf1c39
Miguel Angel Martin Hrdez <miguel.martin@analyticalways.com>
2017-10-05 12:07:16 +0200

    StringCalculatorShould return_1_if_param_is_1

--- StringCalculator/UnitTest1.cs
+++ StringCalculator/UnitTest1.cs
@@ -14,13 +14,25 @@ namespace StringCalculator
             var result = sut.Add(string.Empty);
             result.Should().Be(0);
         }
+
+        [Fact]
+        public void return_1_if_param_is_1()
+        {
+            var sut = new StringCalculator();
+            var result = sut.Add("1");
+            result.Should().Be(1);
+        }
     }
 
     public class StringCalculator
     {
         public int Add(string numbers)
         {
-            return 0;
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+            return int.Parse(numbers);
         }
     }
 }
